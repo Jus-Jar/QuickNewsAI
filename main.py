a429abb33026d8546466e911983c55a2073e94db
Jus-Jar <jaredheeralal04@gmail.com>
2024-10-07 11:40:36 -0400

    Added Bing Search API and code to extract relevant Urls

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
 from groqAPI import loadGroqClient,groqMessageRequest
 from openRouter import loadOpenRouterClient, liquidMessageRequest, llamaMessageRequest
+from bingSearch import bingSearchRequest
 
+
+#LLM Clients
 groqClient = loadGroqClient()
 openRouterClient = loadOpenRouterClient()
 
 message = ""
 while(message != "bye"):
     message = input("Ask me anything:")
+
+    print(bingSearchRequest(message))
     # print(llamaMessageRequest(message,openRouterClient))
-    print(groqMessageRequest(message,groqClient))
+    # print(groqMessageRequest(message,groqClient))
 
 
 
